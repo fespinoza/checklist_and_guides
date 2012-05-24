30bba21e2aea5fa077bd2c3f8634bb341fc05da1
Felipe Espinoza <fespinozacast@gmail.com>
2012-05-24 01:47:20 -0400

    added ruby and chef installation script

---
+++ chef solo guide/install_ruby_and_chef.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+apt-get -y update
+apt-get -y install build-essential zlib1g-dev libssl-dev libreadline6-dev libyaml-dev
+cd /tmp
+wget ftp://ftp.ruby-lang.org/pub/ruby/1.9/ruby-1.9.3-p125.tar.gz
+tar -xvzf ruby-1.9.3-p125.tar.gz
+cd ruby-1.9.3-p125/
+./configure --prefix=/usr/local
+make
+make install
+gem install chef ruby-shadow --no-ri --no-rdoc
